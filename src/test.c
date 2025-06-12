02c595547b27f57843ee935f3dd7d3ff92d4d638
Tattanka <gabthuet@gmail.com>
2025-06-12 10:52:24 +0200

    az

--- src/test.c
+++ src/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-FILE* file = fopen(filename, "r");
+int main(){
+  FILE* file = fopen(filename, "r");
 if (file == NULL) {
   printf("Unable to open file <%s>\n", filename);
   exit(EXIT_FAILURE);
@@ -11,3 +11,4 @@ char line[512];
 while(fgets(line, sizeof(line), file)) {
   // ...
 }
+
