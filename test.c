c94e64062436938c6b7a9771872f30cd61f4a1ab
samspeed44 <sam44dec@gmail.com>
2025-06-12 10:35:26 +0200

    Test1

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE* file = fopen(filename, "r");
+if (file == NULL) {
+  printf("Unable to open file <%s>\n", filename);
+  exit(EXIT_FAILURE);
+}
+
+char line[512];
+while(fgets(line, sizeof(line), file)) {
+  // ...
+}
