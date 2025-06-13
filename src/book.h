815b9af3509748c1deb48ee411608455a42bbf53
nathan <sam44dec44@gmail.com>
2025-06-13 07:54:24 +0200

    commentaire struct chapter

--- src/book.h
+++ src/book.h
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct chapitre{ 
-  int idChapter;
-  char titre[200];
-  char content[520];
-  char choice1;
-  char choice2;
+struct chapitre{      //structure qui contient les informations d’un seul chapitre
+  int idChapter;      // numéro du chapitre
+  char titre[200];    // titre du chapitre
+  char content[520];  // les paragraphes qui composent le chapitre (contenu de l'histoire)
+  char choice1;       // premier choix
+  char choice2;       // deuxième choix
 };
 
-struct Book{
+struct Book{          //contient tous les chapitres (total de 10 chapitres)
   struct chapitre *chapitre;
   };
